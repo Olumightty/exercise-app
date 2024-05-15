0a61976731f8ed8efaa72d4065f69029aad7b47a
Ayoola Olumide Olufemi <ayoolaolumide98@gmail.com>
2024-05-15 11:03:46 +0100

    Improve pagination style and update logic to account for indexes more than length of data

--- components/PaginationButtons.jsx
+++ components/PaginationButtons.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const PaginationButtons = ({ buttonArray, changePage, currentPage }) => {
   return (
     <div className='flex justify-center items-center flex-wrap gap-1'>
-        <button onClick={() => changePage(currentPage - 1)} className='py-2 px-4 border-dimGray border-2 text-dimGray mr-2'>Prev</button>
-        {buttonArray.map((_,index) => <button key={index + 1} onClick={() => changePage(index + 1)} className={`py-2 px-4  mr-2 ${index + 1 == currentPage ? 'bg-primary text-black border-primary border-2' : 'hover:text-primary hover:border-primary border-dimGray border-2 text-dimGray'}`}>{index + 1}</button>)}
+        <button onClick={() => changePage(currentPage - 1)} className='py-2 px-4 text-dimGray mr-2 hover:text-primary'>Prev</button>
+        {buttonArray.map((_,index) => <button key={index + 1} onClick={() => changePage(index + 1)} className={`py-2 px-2 w-16  mr-2 ${index + 1 == currentPage ? 'bg-primary text-black border-primary border-2' : 'hover:text-primary hover:border-primary  text-dimGray'}`}>{index + 1}</button>)}
         <button onClick={() => changePage(currentPage + 1)} className='py-2 px-4 bg-primary text-black mr-2 border-primary border-2'>Next</button>
     </div>
   )
--- components/Suggestions.jsx
+++ components/Suggestions.jsx
@@ -1,15 +1,20 @@
 'use client'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { ExcersiceCard } from "."
 import PaginationButtons from "./PaginationButtons"
 
 
 const Suggestions = ({data}) => {
   //Implementation of Pagination on the client-side
+  
   let pages = []
   const [currentPage, setCurrentPage] = useState(1) 
   const exercisePerPage = 3
   const lastExercise = currentPage * exercisePerPage
+  //this checks if the data length + 3 (since there are 3 items per page) will accomodate the last index value, if last index value is to much for the data length then currentPage will be reset to 1
+  if(lastExercise > data.length + 3){
+    setCurrentPage(1)
+  }
   const firstExercise = lastExercise - exercisePerPage
   const visibleData = data.slice(firstExercise, lastExercise)
   
@@ -26,6 +31,7 @@ const Suggestions = ({data}) => {
     }
     
   }
+  
   return (
     <>
       <div className='lg:grid lg:grid-cols-3 gap-y-8 md:grid md:grid-cols-2 flex flex-col items-center mb-8'>
