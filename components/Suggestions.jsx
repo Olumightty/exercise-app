427b07c68d6e857fa2d0c239bb0c9e6c377c17e9
Ayoola Olumide Olufemi <ayoolaolumide98@gmail.com>
2024-05-29 00:40:18 +0100

    Added React-Paginate to handle Pagination UI

--- components/Suggestions.jsx
+++ components/Suggestions.jsx
@@ -1,13 +1,14 @@
 'use client'
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { ExcersiceCard } from "."
 import PaginationButtons from "./PaginationButtons"
+import ReactPaginate from "react-paginate"
 
 
 const Suggestions = ({data}) => {
   //Implementation of Pagination on the client-side
   
-  let pages = []
+  // let pages = []
   const [currentPage, setCurrentPage] = useState(1) 
   const exercisePerPage = 3
   const lastExercise = currentPage * exercisePerPage
@@ -18,17 +19,13 @@ const Suggestions = ({data}) => {
   const firstExercise = lastExercise - exercisePerPage
   const visibleData = data.slice(firstExercise, lastExercise)
   
-  for(let i = 0; i<Math.ceil(data.length/exercisePerPage); i++){
-    pages.push(i)
-  }
+  // for(let i = 0; i<Math.ceil(data.length/exercisePerPage); i++){
+  //   pages.push(i)
+  // }
+  const pageCount = Math.ceil(data.length / exercisePerPage);
 
-  function changePage(newPage){
-    if(newPage < 1 || newPage > pages.length){
-
-    }
-    else{
-      setCurrentPage(newPage)
-    }
+  function changePage(event){
+    setCurrentPage(event.selected + 1)
     
   }
   
@@ -37,7 +34,24 @@ const Suggestions = ({data}) => {
       <div className='lg:grid lg:grid-cols-3 gap-y-8 md:grid md:grid-cols-2 flex flex-col items-center mb-8'>
         {visibleData.map((da) => <ExcersiceCard key={da.id} id = {da.id} src={da.gifUrl} target={da.target} bodyPart={da.bodyPart} equipment={da.equipment} name={da.name}/>)}
       </div>
-      <PaginationButtons buttonArray = {pages} changePage={changePage} currentPage={currentPage}/>
+     <ReactPaginate
+      breakLabel="..."
+      nextLabel="Next >"
+      onPageChange={changePage}
+      pageRangeDisplayed={5}
+      pageCount={pageCount}
+      previousLabel="< Previous"
+      renderOnZeroPageCount={null}
+      containerClassName="flex justify-center items-center flex-wrap gap-1 shadow-lg shadow-primary/30"
+      pageClassName="py-2 px-4"
+      pageLinkClassName = 'text-dimGray hover:text-primary cursor-pointer'
+      previousLinkClassName="py-2 px-4 text-dimGray hover:text-primary"
+      activeClassName="border-primary border-b-2"
+      activeLinkClassName="text-black"
+      nextLinkClassName="py-2 px-4 text-dimGray hover:text-primary"
+      breakClassName="text-dimGray"
+      disabledLinkClassName="opacity-50"
+     />
       
     </>
     
